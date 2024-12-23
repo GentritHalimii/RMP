9e5a584b17a8b1c0209eb5e3ceb11a43a4e429ac
GentritHalimii <genti.halimi23@gmail.com>
2024-12-23 03:06:07 +0100

    feat: implemented user management

---
+++ RMP.Solution/Migrations/20241225223022_RelationshipsFixes.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMP.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationshipsFixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Universities_UniversityId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Departments_DepartmentId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Universities_UniversityId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Universities_UniversityId",
+                table: "Departments",
+                column: "UniversityId",
+                principalTable: "Universities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Departments_DepartmentId",
+                table: "Users",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Universities_UniversityId",
+                table: "Users",
+                column: "UniversityId",
+                principalTable: "Universities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Universities_UniversityId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Departments_DepartmentId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Universities_UniversityId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Universities_UniversityId",
+                table: "Departments",
+                column: "UniversityId",
+                principalTable: "Universities",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Departments_DepartmentId",
+                table: "Users",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Universities_UniversityId",
+                table: "Users",
+                column: "UniversityId",
+                principalTable: "Universities",
+                principalColumn: "Id");
+        }
+    }
+}
